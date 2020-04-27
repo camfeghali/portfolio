28a6a5d4385ce3fc0e9a0896f06a795fb7e3224d
camfeghali <camfeg@gmail.com>
2020-04-27 13:22:15 -0400

    createElement function done

--- src/js/App.js
+++ src/js/App.js
@@ -6,9 +6,18 @@ import { MunkeyReact } from "./dom";
 export const App = () => {
 
     const element = (
-        <div>
-            <h1>Hello, from element</h1>
-        </div>
+      <div>
+      <h1 className="header">Hello Tiny React!</h1>
+      <h2>(coding nirvana)</h2>
+      <div>nested 1<div>nested 1.1</div></div>
+      <h3>(OBSERVE: This will change)</h3>
+      {2 == 1 && <div>Render this if 2==1</div>}
+      {2 == 2 && <div>{2}</div>}
+      <span>This is a text</span>
+      <button onClick={() => alert("Hi!")}>Click me!</button>
+      <h3>This will be deleted</h3>
+      2,3
+    </div>
     )
     console.log(element)
 }
