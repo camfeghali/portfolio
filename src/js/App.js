f4a6ea9827d395be4e55bd01d24a2f33a2941499
camfeghali <camfeg@gmail.com>
2020-05-08 14:31:30 -0400

    made navbar

--- src/js/App.js
+++ src/js/App.js
@@ -2,7 +2,7 @@
 
 import { MunkeyReact } from "./dom";
 import mexicoCamille from "../img/mexico-camille.png";
-import { SocialLinks, FavoriteTech, Heading, BlogPost, Footer } from './FunctionalComps.js'
+import { NavBar, SocialLinks, FavoriteTech, Heading, BlogPost, Footer } from './FunctionalComps.js'
 import Accordion from './Accordion.js'
 import { AccordionElement } from './Accordion.js'
 import { 
@@ -52,7 +52,8 @@ class AncestorComponent extends MunkeyReact.Component {
 function Content (props) {
   return(
     <div className="content-column">
-      <h2 style="margin-bottom: 2em;">Thanks for checking out my profile!</h2>
+      <NavBar />
+      <h2 id="about" style="margin-bottom: 2em; color:rgba(60,160,255,1);">Thanks for checking out my profile!</h2>
       <img src={mexicoCamille} className="munkey-logo"></img>
       <SocialLinks />
       <h2 style="color:rgba(60,164,157,1)"> Software Engineer @ <a className="honeyco-link">HoneyCO</a> </h2>
@@ -60,14 +61,14 @@ function Content (props) {
       <hr/>
       <FavoriteTech />
       <hr/>
-      <Heading title="Professional Projects"/>
+      <Heading id="professional-projects" title="Professional Projects"/>
       <Accordion>
         <AccordionElement label="Load Shedding Agent 2.0" content={lsaDescription}/>
         <AccordionElement label="Rona" content={ronaDescription}/>
         <AccordionElement label="Agritect Designer" content={agritectureDesignerDescription}/>
         <AccordionElement label="Cancercare.org" content={cancercareDescription}/>
       </Accordion>
-      <Heading title="Personal Projects"/>
+      <Heading id="personal-projects" title="Personal Projects"/>
       <Accordion>
         <AccordionElement label="MunkeyReact, a ReactJS Clone" content={munkeyReactDescription}/>
         <AccordionElement label="Synced" content={syncedDescription}/>
@@ -75,7 +76,7 @@ function Content (props) {
         <AccordionElement label="Caravan" content={caravanDescription}/>
         <AccordionElement label="NodeJS | Babel | Webpack | Express boilerplate" content={nodeBoilerplateDescription}/>
       </Accordion>
-      <Heading title="Blog Posts"/>
+      <Heading id="blog" title="Blog Posts"/>
       <BlogPost title="JavaScript, my favorite compiled language" link="https://medium.com/javascript-in-plain-english/javascript-my-favorite-compiled-language-a-blog-about-scope-2012071aac86"/>
       <BlogPost title="How Closures lead to Modules in JavaScript" link="https://medium.com/javascript-in-plain-english/javascript-from-closure-to-modules-21d1ba30ab36?source=your_stories_page---------------------------"/>
       <BlogPost title="Object Oriented Programming and the Prototype Chain in JavaScript." link="https://medium.com/javascript-in-plain-english/object-oriented-programming-and-the-prototype-chain-in-javascript-d71032a30324?source=your_stories_page---------------------------"/>
