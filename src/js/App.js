f070a96e8bcf53d4f5c267bd1add0dca1301e407
Camille Feghali <camille@DESKTOP-8UIB21N.localdomain>
2020-11-10 22:46:26 +0200

    added rarecast

--- src/js/App.js
+++ src/js/App.js
@@ -5,7 +5,7 @@ import mexicoCamille from "../img/mexico-camille.png";
 import { NavBar, SocialLinks, FavoriteTech, Heading, BlogPost, Footer } from './FunctionalComps.js'
 import Accordion from './Accordion.js'
 import { AccordionElement } from './Accordion.js'
-import { 
+import {
   lsaDescription,
   munkeyReactDescription,
   ronaDescription,
@@ -14,7 +14,8 @@ import {
   syncedDescription,
   ketoMeDescription,
   nodeBoilerplateDescription,
-  caravanDescription
+  caravanDescription,
+  RARECastDescription
 } from './ProjectsDescriptions.js'
 
 const root = document.getElementById("root");
@@ -23,7 +24,7 @@ export const App = () => {
 
   let greetingMessage = "Welcome to MunkeyReact, a ReactJS clone"
 
-  MunkeyReact.render(<AncestorComponent greetingMessage={greetingMessage}/>, root)
+  MunkeyReact.render(<AncestorComponent greetingMessage={greetingMessage} />, root)
 };
 
 class AncestorComponent extends MunkeyReact.Component {
@@ -35,16 +36,16 @@ class AncestorComponent extends MunkeyReact.Component {
     this.handleOnScroll = this.handleOnScroll.bind(this)
   }
 
-  handleOnScroll () {
+  handleOnScroll() {
     console.log("Helooo")
   }
 
   render() {
-    return(
+    return (
       <div className="ancestor-wrapper">
         <div></div>
         <div className="content-wrapper">
-          <Content onClick={this.handleOnScroll} file="App.js"/>
+          <Content onClick={this.handleOnScroll} file="App.js" />
         </div>
         <div></div>
       </div>
@@ -53,41 +54,42 @@ class AncestorComponent extends MunkeyReact.Component {
 }
 
 
-function Content (props) {
-  return(
+function Content(props) {
+  return (
     <div id="about" className="content-column">
       <NavBar />
-      <h2  style="margin-bottom: 2em; color:rgba(60,160,255,1);">Thanks for checking out my website!</h2>
+      <h2 style="margin-bottom: 2em; color:rgba(60,160,255,1);">Thanks for checking out my website!</h2>
       <img src={mexicoCamille} className="munkey-logo"></img>
       <SocialLinks />
       <h2 style="color:rgba(60,164,157,1)"> Software Engineer @ <a className="honeyco-link">HoneyCO</a> </h2>
       <h2 className="css-style">This page is built with a <a className="munkey-react-link">ReactJS clone</a> I made</h2>
-      <hr/>
+      <hr />
       <FavoriteTech />
-      <hr/>
-      <Heading id="professional-projects" title="Professional Projects"/>
+      <hr />
+      <Heading id="professional-projects" title="Professional Projects" />
       <Accordion>
-        <AccordionElement label="Load Shedding Agent 2.0" content={lsaDescription}/>
-        <AccordionElement label="Rona" content={ronaDescription}/>
-        <AccordionElement label="Agritect Designer" content={agritectureDesignerDescription}/>
-        <AccordionElement label="Cancercare.org" content={cancercareDescription}/>
+        <AccordionElement label="RARECast" content={RARECastDescription} />
+        <AccordionElement label="Load Shedding Agent 2.0" content={lsaDescription} />
+        <AccordionElement label="Rona" content={ronaDescription} />
+        <AccordionElement label="Agritect Designer" content={agritectureDesignerDescription} />
+        <AccordionElement label="Cancercare.org" content={cancercareDescription} />
       </Accordion>
-      <Heading id="personal-projects" title="Personal Projects"/>
+      <Heading id="personal-projects" title="Personal Projects" />
       <Accordion>
-        <AccordionElement label="MunkeyReact, a ReactJS Clone" content={munkeyReactDescription}/>
-        <AccordionElement label="Synced" content={syncedDescription}/>
-        <AccordionElement label="KetoMe" content={ketoMeDescription}/>
-        <AccordionElement label="Caravan" content={caravanDescription}/>
-        <AccordionElement label="NodeJS | Babel | Webpack | Express boilerplate" content={nodeBoilerplateDescription}/>
+        <AccordionElement label="MunkeyReact, a ReactJS Clone" content={munkeyReactDescription} />
+        <AccordionElement label="Synced" content={syncedDescription} />
+        <AccordionElement label="KetoMe" content={ketoMeDescription} />
+        <AccordionElement label="Caravan" content={caravanDescription} />
+        <AccordionElement label="NodeJS | Babel | Webpack | Express boilerplate" content={nodeBoilerplateDescription} />
       </Accordion>
-      <Heading id="blog" title="Blog Posts"/>
-      <BlogPost title="JavaScript, my favorite compiled language" link="https://medium.com/javascript-in-plain-english/javascript-my-favorite-compiled-language-a-blog-about-scope-2012071aac86"/>
-      <BlogPost title="How Closures lead to Modules in JavaScript" link="https://medium.com/javascript-in-plain-english/javascript-from-closure-to-modules-21d1ba30ab36?source=your_stories_page---------------------------"/>
-      <BlogPost title="Object Oriented Programming and the Prototype Chain in JavaScript." link="https://medium.com/javascript-in-plain-english/object-oriented-programming-and-the-prototype-chain-in-javascript-d71032a30324?source=your_stories_page---------------------------"/>
-      <BlogPost title="JavasScript Promises and the Micro Task Queue." link="https://medium.com/javascript-in-plain-english/javasscript-promises-and-the-micro-task-queue-6111f7452f05"/>
-      <BlogPost title="Asynchronous JavaScript Explained" link="https://medium.com/javascript-in-plain-english/asynchronous-javascript-explained-a4c1133f5544"/>
-      <BlogPost title="Step by Step guide to set up a basic full-stack app with Rails and Vanilla Javascript." link="https://medium.com/@camfeg/step-by-step-guide-to-set-up-a-basic-full-stack-app-with-rails-and-vanilla-javascript-12ae33ff0c64"/>
-      <BlogPost title="Dynamic method definition with ruby’s .define_method" link="https://medium.com/@camfeg/dynamic-method-definition-with-rubys-define-method-b3ffbbee8197?source=your_stories_page---------------------------"/>
+      <Heading id="blog" title="Blog Posts" />
+      <BlogPost title="JavaScript, my favorite compiled language" link="https://medium.com/javascript-in-plain-english/javascript-my-favorite-compiled-language-a-blog-about-scope-2012071aac86" />
+      <BlogPost title="How Closures lead to Modules in JavaScript" link="https://medium.com/javascript-in-plain-english/javascript-from-closure-to-modules-21d1ba30ab36?source=your_stories_page---------------------------" />
+      <BlogPost title="Object Oriented Programming and the Prototype Chain in JavaScript." link="https://medium.com/javascript-in-plain-english/object-oriented-programming-and-the-prototype-chain-in-javascript-d71032a30324?source=your_stories_page---------------------------" />
+      <BlogPost title="JavasScript Promises and the Micro Task Queue." link="https://medium.com/javascript-in-plain-english/javasscript-promises-and-the-micro-task-queue-6111f7452f05" />
+      <BlogPost title="Asynchronous JavaScript Explained" link="https://medium.com/javascript-in-plain-english/asynchronous-javascript-explained-a4c1133f5544" />
+      <BlogPost title="Step by Step guide to set up a basic full-stack app with Rails and Vanilla Javascript." link="https://medium.com/@camfeg/step-by-step-guide-to-set-up-a-basic-full-stack-app-with-rails-and-vanilla-javascript-12ae33ff0c64" />
+      <BlogPost title="Dynamic method definition with ruby’s .define_method" link="https://medium.com/@camfeg/dynamic-method-definition-with-rubys-define-method-b3ffbbee8197?source=your_stories_page---------------------------" />
       <Footer />
     </div>
   );
--- src/js/ProjectsDescriptions.js
+++ src/js/ProjectsDescriptions.js
@@ -5,9 +5,9 @@ import { MunkeyReact } from "./dom";
 export const munkeyReactDescription = (
     <div className="project-description-wrapper">
         <p>
-            MunkeyReact was built following the guidance of Rajesh Pillai's 
+            MunkeyReact was built following the guidance of Rajesh Pillai's
             course on Udemy, <a href="https://www.udemy.com/course/javascript-deep-dive-code-your-own-react-library/"><i>'
-            JavaScript Master Class - Build your own React from scratch'</i></a>. A challenging yet rewarding endeavor that helped demistify 
+            JavaScript Master Class - Build your own React from scratch'</i></a>. A challenging yet rewarding endeavor that helped demistify
             React Magic and understand its main building blocks; the VDOM, createElement and Diffing. <a href="https://github.com/camfeghali/munkey-react"><u>Repo</u></a>.
         </p>
     </div>
@@ -16,8 +16,8 @@ export const munkeyReactDescription = (
 export const lsaDescription = (
     <div className="project-description-wrapper">
         <p>
-            <u>Client</u>: <a href="http://www1.cuny.edu/sites/sustainable/"> CUNY Conserves.</a><br/>
-            LSA 2.0 is a Python/Django application deployed on a Raspberry Pi to shed peak electricity load in government buildings. 
+            <u>Client</u>: <a href="http://www1.cuny.edu/sites/sustainable/"> CUNY Conserves.</a><br />
+            LSA 2.0 is a Python/Django application deployed on a Raspberry Pi to shed peak electricity load in government buildings.
             The team established BACnet/IP communication to edit points on field controllers deployed in LaGuardia Community College.
             We Developed the user interface to manage the agent’s settings and configuration as well as track events.
             One main challenge is CI/CD and standardizing the deployment process via Windows and Linux machines.
@@ -25,10 +25,21 @@ export const lsaDescription = (
     </div>
 )
 
+export const RARECastDescription = (
+    <div className="project-description-wrapper">
+        <p>
+            <u>Client</u>: <a href="https://globalgenes.org/rare-cast/"> GlobalGenes.</a><br />
+            The goal of the project was to allow GlobalGenes admins to display over 300 podcasts hosted on ART19.
+            The RARECast project was developed using React, GraphQL, Ruby on Rails and the <u><a href="https://art19.com/api-docs/index.html?tag=v1&version=1.0.0">ART19 API</a></u>.
+            Rails and the ActiveAdmin Gem were used to build a Content Management System coupled with sync scripts to interface with the ART19 to synchronize data and recently uploaded podcasts.
+        </p>
+    </div>
+)
+
 export const ronaDescription = (
     <div className="project-description-wrapper">
         <p>
-            <u>HoneyCO</u> project.<br/>
+            <u>HoneyCO</u> project.<br />
             Built with Flutter, a Rails back-end and the official COVID-19 API.
             A anonymous social / location based COVID-19 mobile application that hopes to keep you well-connected & informed in one centralized place.
         </p>
@@ -38,8 +49,8 @@ export const ronaDescription = (
 export const agritectureDesignerDescription = (
     <div className="project-description-wrapper">
         <p>
-            <u>Client</u>: <a href="https://www.agritecture.com/"> Agritecture.</a><br/>
-            A ReactJS application that integrates with Google Cloud Functions to generate a Urban Farm feasibility report. 
+            <u>Client</u>: <a href="https://www.agritecture.com/"> Agritecture.</a><br />
+            A ReactJS application that integrates with Google Cloud Functions to generate a Urban Farm feasibility report.
             Authenticates with Firebase to store and manage user project data.
             Interracts with Google Cloud Functions to compute model values.
             State management is achieved using the React Context API,
@@ -51,10 +62,10 @@ export const agritectureDesignerDescription = (
 export const cancercareDescription = (
     <div className="project-description-wrapper">
         <p>
-            <u>Client</u>: <a href="https://www.cancercare.org/"> CancerCare.org</a><br/>
+            <u>Client</u>: <a href="https://www.cancercare.org/"> CancerCare.org</a><br />
             My responsiblities include Feature development, support & maintenance in Ruby on Rails.
-            Features include: 
-            
+            Features include:
+
             <ol>
                 <li>
                     - Implementing Two-Factor Authentication with the Twilio API.
@@ -73,10 +84,10 @@ export const cancercareDescription = (
 export const syncedDescription = (
     <div className="project-description-wrapper">
         <p>
-            <u>Repo</u>: <a href="https://github.com/camfeghali/synced-frontend"> <u>Frontend</u> </a> | <a href="https://github.com/camfeghali/synced-backend"> <u>Backend</u> </a><br/>
-            Synced is a fullstack web application built with Ruby on Rails and ReactJS. 
-            The purpose of this app is to synchronize music so users can listen to songs 
-            at the same time regardless of their location. It is my final project as part of 
+            <u>Repo</u>: <a href="https://github.com/camfeghali/synced-frontend"> <u>Frontend</u> </a> | <a href="https://github.com/camfeghali/synced-backend"> <u>Backend</u> </a><br />
+            Synced is a fullstack web application built with Ruby on Rails and ReactJS.
+            The purpose of this app is to synchronize music so users can listen to songs
+            at the same time regardless of their location. It is my final project as part of
             Flatiron School's Software Engineering Bootcamp.
         </p>
     </div>
@@ -85,10 +96,10 @@ export const syncedDescription = (
 export const ketoMeDescription = (
     <div className="project-description-wrapper">
         <p>
-            <u>Repo</u>: <a href="https://github.com/camfeghali/synced-frontend"> <u>Frontend</u> </a> | <a href="https://github.com/camfeghali/synced-backend"> <u>Backend</u> </a><br/>
-            KetoMe is a fullstack web application built with Ruby on Rails during my time at the Flatiron School. 
+            <u>Repo</u>: <a href="https://github.com/camfeghali/synced-frontend"> <u>Frontend</u> </a> | <a href="https://github.com/camfeghali/synced-backend"> <u>Backend</u> </a><br />
+            KetoMe is a fullstack web application built with Ruby on Rails during my time at the Flatiron School.
             KetoMe integrates the <a href="https://developer.edamam.com/">Edamam API</a>, and allows users to look for recipes,
-            and keep track of their nutritional intake to monitor their performance as 
+            and keep track of their nutritional intake to monitor their performance as
             it pertains to the keto diet, more on the diet <a href="https://www.healthline.com/nutrition/ketogenic-diet-101"><u>here</u>.</a>
         </p>
     </div>
@@ -97,8 +108,8 @@ export const ketoMeDescription = (
 export const nodeBoilerplateDescription = (
     <div className="project-description-wrapper">
         <p>
-            <u>Repo</u> <a href="https://github.com/camfeghali/express-babel-webpack-boilerplate"></a><br/>
-            I went through the process of configuring a NodeJS, Webpack, Babel app served via Express. 
+            <u>Repo</u> <a href="https://github.com/camfeghali/express-babel-webpack-boilerplate"></a><br />
+            I went through the process of configuring a NodeJS, Webpack, Babel app served via Express.
             I used it as the foundation for the React Clone. Going through that set up helped demistify Webpack and Node / Express setup, something that had always been done for me by my peers ❤.
             Now I can actually debug a Webpack config !
         </p>
@@ -114,9 +125,9 @@ export const nodeBoilerplateDescription = (
 export const caravanDescription = (
     <div className="project-description-wrapper">
         <p>
-        <u>Repo</u>: <a href="https://github.com/camfeghali/caravan-frontend"> <u>Frontend</u> </a> | <a href="https://github.com/camfeghali/caravan-backend"> <u>Backend</u> </a><br/>
-            Another student project, Caravan is a fullstack web application built with Ruby on Rails and ReactJS. It is a E-commerce platform 
-            that allows users to buy & sell second hand goods close to them. 
+            <u>Repo</u>: <a href="https://github.com/camfeghali/caravan-frontend"> <u>Frontend</u> </a> | <a href="https://github.com/camfeghali/caravan-backend"> <u>Backend</u> </a><br />
+            Another student project, Caravan is a fullstack web application built with Ruby on Rails and ReactJS. It is a E-commerce platform
+            that allows users to buy & sell second hand goods close to them.
             It integrates the <a href="https://react-leaflet.js.org/">Leaflet library</a>  and the <a href="https://developer.mapquest.com/documentation/leaflet-plugins/">Mapquest API</a> to show users where items are being sold.
         </p>
     </div>
